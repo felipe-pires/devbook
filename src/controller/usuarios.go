9f3ea8b2aa95d95ab651794e12e25bd14dadb3f1
Felipe dos Santos <felipedossantospires01@gmail.com>
2023-02-23 16:28:22 -0300

    criar usuario

--- src/controller/usuarios.go
+++ src/controller/usuarios.go
@@ -1,28 +1,67 @@
 package controller
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/model"
+	"api/src/repository"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-//CriarUsuario insere um usuario no banco de dados
+// CriarUsuario insere um usuario no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Criando Usuario..."))
+
+	body, erro := ioutil.ReadAll(r.Body)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario model.Usuario
+
+	if erro = json.Unmarshal(body, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	defer db.Close()
+
+	repositorio := repository.UsuarioRepository(db)
+	ID, erro := repositorio.Criar(usuario)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("Id inserido: %d", ID)))
 }
 
-//BuscarUsuarios busca todos os usuarios
+// BuscarUsuarios busca todos os usuarios
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscando usuarios..."))
 }
 
-//BuscarUsuario busca um usuario pelo id
+// BuscarUsuario busca um usuario pelo id
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscando usuario com id: "))
 }
 
-//AtualizarUsuario altera informacoes de um usuario
+// AtualizarUsuario altera informacoes de um usuario
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Atualizando usuario..."))
 }
 
-//DeletarUsuario deleta um usuario pelo id
+// DeletarUsuario deleta um usuario pelo id
 func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Deletando usuario com id: "))
 }
---
+++ src/repository/usuarios.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"api/src/model"
+	"database/sql"
+)
+
+type usuarios struct {
+	db *sql.DB
+}
+
+// UsuarioRepository criar repositorio de usuarios
+func UsuarioRepository(db *sql.DB) *usuarios {
+	return &usuarios{db}
+}
+
+// Criar insere usuario no banco de dados
+func (repositorio usuarios) Criar(usuario model.Usuario) (uint64, error) {
+
+	statement, erro := repositorio.db.Prepare("insert into usuarios(nome, nick, email, senha) values (?, ?, ?, ?)")
+
+	if erro != nil {
+		return 0, erro
+	}
+
+	defer statement.Close()
+
+	result, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+
+	if erro != nil {
+		return 0, erro
+	}
+
+	ID, erro := result.LastInsertId()
+
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ID), nil
+
+	return 0, nil
+}
